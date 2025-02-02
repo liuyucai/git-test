5f5af65e15075b69cbee2d789ec66ccdd72c6f62
lyc <2395783668@qq.com>
2025-02-02 17:36:29 +0800

    测试dev暂存保存

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -2,9 +2,11 @@ package $com.qwen;
 
 /**
  * Hello world!
- *
+ * 测试修改主分支
+ * 测试主分支修改2
+ * 测试主分支修改3
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
